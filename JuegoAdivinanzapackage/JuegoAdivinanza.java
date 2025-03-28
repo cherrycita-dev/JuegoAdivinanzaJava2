d26edb97f22101ef3422f110dfff1adf35adc2b1
cherrycita-dev <pereznancyesmeralda@gmail.com>
2025-03-27 22:30:41 -0600

    Add files via upload
    Se integra clase principal para la homologación de las funciones

---
+++ JuegoAdivinanzapackage/JuegoAdivinanza.java
@@ -0,0 +1,10 @@
+package JuegoAdivinanzapackage;
+
+public class JuegoAdivinanza {
+
+	public static void main(String[] args) {
+		//metodo principal encargado de juntar todas las funciones compañeras
+
+	}
+
+}
