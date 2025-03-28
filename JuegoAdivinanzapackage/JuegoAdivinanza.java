a502d4505fee8900db2bc8d912ca0eb9067a0a85
cherrycita-dev <pereznancyesmeralda@gmail.com>
2025-03-27 22:32:40 -0600

    Update JuegoAdivinanza.java
    main principal

--- JuegoAdivinanzapackage/JuegoAdivinanza.java
+++ JuegoAdivinanzapackage/JuegoAdivinanza.java
@@ -1,10 +1,48 @@
-package JuegoAdivinanzapackage;
+import java.util.Scanner;
+import java.util.Random;
 
-public class JuegoAdivinanza {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-	public static void main(String[] args) {
-		//metodo principal encargado de juntar todas las funciones compañeras
+        int numeroSecreto = 0 //sin determinar
+        int intentosMaximos = 10;
 
-	}
+        mostrarMensajeBienvenida(intentosMaximos);
 
+        boolean adivinado = jugar(scanner, numeroSecreto, intentosMaximos);
+
+        if (!adivinado) {
+            mostrarMensajeDerrota(numeroSecreto);
+        }
+
+        scanner.close();
+    }
+
+    private static int generarNumeroSecreto(Random random) {
+        // Método vacío
+        return 0;
+    }
+
+    private static void mostrarMensajeBienvenida(int intentosMaximos) {
+        // Método vacío
+    }
+
+    private static boolean jugar(Scanner scanner, int numeroSecreto, int intentosMaximos) {
+        // Método vacío
+        return false;
+    }
+
+    private static void mostrarMensajeVictoria(int intentos) {
+        // Método vacío
+    }
+
+    private static void mostrarMensajeDerrota(int numeroSecreto) {
+        // Método vacío
+    }
+
+    private static void darPista(int intento, int numeroSecreto) {
+        // Método vacío
+    }
 }
